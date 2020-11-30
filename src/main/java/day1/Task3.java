370607b70b0692914ff98a5f2812c3f123c66c2e
Vadim Khublarian <vadimjedi@gmail.com>
2020-11-30 13:11:53 +0300

    complete day1 tasks

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        for (int count = 0; count < 10; count++) {
+            System.out.println("JAVA");
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 3;
+        for (int count = 1; count <= 9; count++) {
+            // System.out.println(count + " x " + k + " = " + count * k);
+            System.out.printf("%d x %d = %d\n", count, k, count * k);
+        }
     }
 }
