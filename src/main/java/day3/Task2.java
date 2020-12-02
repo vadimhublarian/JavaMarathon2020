605f9847c3f518182324f56ee53327912461d9a8
Vadim Khublarian <vadimjedi@gmail.com>
2020-12-02 19:37:04 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double dividend, divisor;
 
+        while (true) {
+            dividend = scanner.nextDouble();
+            divisor = scanner.nextDouble();
+            if (divisor == 0) break;
+
+            System.out.println(dividend / divisor);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double dividend, divisor;
 
+        for (int counter = 0; counter < 5; counter++) {
+            dividend = scanner.nextDouble();
+            divisor = scanner.nextDouble();
+            if (divisor == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(dividend / divisor);
+        }
     }
 }
