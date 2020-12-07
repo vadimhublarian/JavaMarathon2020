541f797b71594d45892c1c3f52c16a547b1c743e
Vadim Khublarian <vadimjedi@gmail.com>
2020-12-07 17:57:43 +0300

    - added the fixed test in the Day7PlayerTest - optimized the run method in Player the class

--- src/main/java/day7/Player.java
+++ src/main/java/day7/Player.java
@@ -6,34 +6,33 @@ package day7;
  */
 public class Player {
 
-    private int stamina;
     public static final int MAX_STAMINA = 100;
     public static final int MIN_STAMINA = 0;
     public static int countPlayers = 0;
+    private int stamina;
 
     public Player(int stamina) {
         this.stamina = stamina;
         if (countPlayers < 6) countPlayers++;
     }
 
-    public int getStamina() {
-        return stamina;
-    }
-
     public static int getCountPlayers() {
         return countPlayers;
-    }
-
-    public void run() {
-        if (this.stamina > 0) {
-            this.stamina--;
-        }
-        if (this.stamina == 0 && countPlayers > 0) countPlayers--;
     }
 
     public static void info() {
         if (countPlayers < 6)
             System.out.println("Команды неполные. На поле еще есть " + (6 - countPlayers) + " свободных мест");
         else System.out.println("На поле нет свободных мест");
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void run() {
+        if (this.stamina == 0) return; // exit 0;
+        if (this.stamina > 0) this.stamina--;
+        if (this.stamina == 0 && countPlayers > 0) countPlayers--;
     }
 }
