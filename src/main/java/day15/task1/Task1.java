17a96cf9476bba7c422cfe0e196d269f0e9fbbf1
Vadim Khublarian <vadimjedi@gmail.com>
2020-12-15 15:32:18 +0300

    complete day15 tasks

--- src/main/java/day15/task1/Task1.java
+++ src/main/java/day15/task1/Task1.java
@@ -1,7 +1,32 @@
 package day15.task1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        try {
+            File inputFile = new File("src/main/resources/shoes.csv");
+            Scanner scanner = new Scanner(inputFile);
+            File outputFile = new File("src/main/resources/missing_shoes.txt");
+            PrintWriter printWriter = new PrintWriter(outputFile);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] shoe = line.split(";");
+
+                if (Integer.parseInt(shoe[2]) == 0) {
+                    printWriter.println(Arrays.toString(shoe));
+                }
+            }
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
 
     }
 }
